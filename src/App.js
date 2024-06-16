edfe0e1c677460d9bc86134c27820deb5318e249
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-16 15:26:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import { jwtDecode } from "jwt-decode";
 import { UserContext } from "./contexts/UserContext";
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 function App() {
   const [user, setUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
+  const signInButton = useRef();
 
   const handleCallBack = (res) => {
     // console.log("Encoded token:" + res.credential);//*TESTING
@@ -23,7 +24,7 @@ function App() {
         "107023321751-1pol1c8vftuek7gr9ai58q56c6dle35i.apps.googleusercontent.com",
       callback: handleCallBack,
     });
-    google.accounts.id.renderButton(document.getElementById("SignIn"), {
+    google.accounts.id.renderButton(signInButton.current, {
       theme: "outline",
       size: "large",
     });
@@ -31,7 +32,12 @@ function App() {
 
   return (
     <UserContext.Provider value={[user, setUser]}>
-      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} user={user} />
+      <Header
+        loggedIn={loggedIn}
+        setLoggedIn={setLoggedIn}
+        user={user}
+        signInButton={signInButton}
+      />
     </UserContext.Provider>
   );
 }
