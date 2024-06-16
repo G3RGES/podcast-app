10eb026b51e40f74a4cf42c811f88c3f1f19892b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-16 15:11:08 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ loggedIn, setLoggedIn }) => {
+  const [user, setUser] = useContext(UserContext);
+
+  const handleLogOut = () => {
+    setLoggedIn(false);
+    setUser({});
+  };
+
+  return (
+    <>
+      {loggedIn ? (
+        <>
+          <div className="flex flex-row items-center">
+            <img className="" src={user.picture} alt="user_img" />
+            <button
+              className="border py-1 px-3 rounded-lg bg-blue-500 text-white font-semibold my-4"
+              onClick={handleLogOut}
+            >
+              Log out
+            </button>
+          </div>
+          <h1 className="text-3xl font-medium ">HI there, {user.given_name}</h1>
+        </>
+      ) : (
+        <div className="mt-8">
+          <div id="SignIn"></div>
+        </div>
+      )}
+      <h1 className="text-5xl text-center font-semibold mt-3 py-2">
+        Podcasts App
+      </h1>
+    </>
+  );
 };
 
 export default Header;
