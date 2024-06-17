9358f610198694d2b4ac7ca4fcd1660c387bafc3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-18 02:56:54 +0300

    Update Episode.jsx

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Episode = ({ title, pubDate, mp3, link }) => {
   return (
-    <div>
-      <p>{title}</p>
+    <div
+      className="pl-2 mt-2 mb-4 border w-3/4 max-w-2xl py-2 px-5
+     rounded-lg flex flex-col  justify-between"
+    >
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:opacity-70 flex flex-row items-start justify-start"
+      >
+        <p>{title}</p>
+      </a>
+      <audio src={mp3} controls className="my-4 w-full px-2" />
+      <p className="text-center">{pubDate}</p>
     </div>
   );
 };
